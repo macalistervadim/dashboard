db6586e68b90304685f0404b9e55a2781d0f79ab
macalistervadim <macalistervadim@yandex.ru>
2023-08-04 12:58:07 +0600

    Add notification comments

--- dashboard/landing/signals.py
+++ dashboard/landing/signals.py
@@ -1,9 +1,17 @@
 from django.dispatch import Signal, receiver
+from django.db.models.signals import post_save
 
-from .utilities import send_activation_notification
+from .utilities import send_activation_notification, send_new_comment_notification
+from .models import Comment
 
 post_register = Signal()
 
 @receiver(post_register)
 def post_register_dispatcher(sender, **kwargs):
     send_activation_notification(kwargs['instance'])
+
+@receiver(post_save, sender=Comment)
+def post_save_dispatcher(sender, **kwargs):
+    author = kwargs['instance'].bb.author
+    if kwargs['created'] and author.send_messages:
+        send_new_comment_notification(kwargs['instance'])
--- dashboard/landing/utilities.py
+++ dashboard/landing/utilities.py
@@ -10,7 +10,7 @@ def send_activation_notification(user):
     if settings.ALLOWED_HOSTS:
         host = 'http://' + settings.ALLOWED_HOST[0]
     else:
-        host = 'http://localhost:8000'
+        host = 'http://127.0.0.1:8000/'
 
     context = {'user': user, 'host': host, 'sign': signer.sign(user.username)}
     subject = render_to_string('email/activation_letter_subject.txt', context)
@@ -19,3 +19,15 @@ def send_activation_notification(user):
 
 def get_timestamp_path(instance, filename):
     return '%s%s' % (datetime.now().timestamp(), splitext(filename)[1])
+
+def send_new_comment_notification(comment):
+    if settings.ALLOWED_HOSTS:
+        host = 'http://' + settings.ALLOWED_HOST[0]
+    else:
+        host = 'http://localhost:8000'
+
+    author = comment.bb.author
+    context = {'author': author, 'host': host, 'comment': comment}
+    subject = render_to_string('email/new_comment_letter_subject.txt', context)
+    body_text = render_to_string('email/new_comment_letter_body.txt', context)
+    author.email_user(subject, body_text)
